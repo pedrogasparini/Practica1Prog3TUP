52db85227c1e57350a0e15f878d39c5d8868d980
Pedro Ortiz Gasparini <pedrogasparini99@gmail.com>
2024-04-10 12:42:28 -0300

    Ejercicio 5 resuelto

---
+++ Web/Controllers/Ej5Controller.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+
+    public class Ej5Controller : ControllerBase
+    {
+        [HttpGet()]
+        public string Get([FromQuery] string dia)
+        {
+
+            
+            dia = dia.Trim();
+
+            
+            dia = dia.ToLower();
+
+            
+            if (dia.Equals("sábado") || dia.Equals("sabado") || dia.Equals("domingo"))
+            {
+                return ("es fin de semana");
+            }
+            
+            else if (dia.Equals("lunes") || dia.Equals("martes") || dia.Equals("miércoles") || dia.Equals("miercoles") || dia.Equals("jueves") || dia.Equals("viernes"))
+            {
+                return ("NO es fin de semana");
+            }
+            else
+            {
+                return ("el nombre del día ingresado no es valido");
+            }
+        }
+    }
+}
+    
+
