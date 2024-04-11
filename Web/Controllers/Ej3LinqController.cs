312aa84c8f39b1c190b3f5a50396e2b7edd1fb4c
pedrogasparini <pedrogasparini99@gmail.com>
2024-04-11 15:31:19 -0300

    Ejercicio 3 LINQ

---
+++ Web/Controllers/Ej3LinqController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej3LinqController : ControllerBase
+    {
+        [HttpGet()]
+        public List<string> Get([FromQuery] List<string> word)
+        {
+
+            var query =
+                from letter in word
+                where letter.ToLower()[0] == 'b' | letter.ToLower().Last() == 'r'
+                select letter.ToLower();
+
+            return query.ToList();
+        }
+    }
+}
+
