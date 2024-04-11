1baa5774d23a3d0b2e4322a6e16478cb0258f22c
pedrogasparini <pedrogasparini99@gmail.com>
2024-04-11 15:11:15 -0300

    Ejercicio 2 LINQ

---
+++ Web/Controllers/Ej2LinqController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej2LinqController : ControllerBase
+    {
+        [HttpGet()]
+        public string[] Get([FromQuery] List<string> word)
+        {
+            var query =
+                from w in word
+                where w.Length >= 5
+                select w.ToUpper();
+            
+            return query.ToArray();
+
+        }
+    }
+}
+
