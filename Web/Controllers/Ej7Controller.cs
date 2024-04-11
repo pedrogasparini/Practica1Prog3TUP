77d1d957ed8e3a46af3d455c3a633d7c5d6d4018
pedrogasparini <pedrogasparini99@gmail.com>
2024-04-11 10:24:32 -0300

    Practica 1 Ejercicio 7

---
+++ Web/Controllers/Ej7Controller.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej7Controller : ControllerBase
+    {
+        [HttpGet()]
+        public List<int> Get()
+        {
+           List<int> result = [];
+
+
+           for (int i = 1; i <= 100; i++)
+            {
+                result.Add(i);
+            }
+
+            return (result);   
+        }
+    }
+}
