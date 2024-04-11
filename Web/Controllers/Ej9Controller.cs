98b1403c6404a7299ce3ffefc73b6be5c25269ba
pedrogasparini <pedrogasparini99@gmail.com>
2024-04-11 11:42:06 -0300

    Practica 1 Ejercicio 9

---
+++ Web/Controllers/Ej9Controller.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej9Controller : ControllerBase
+    {
+        [HttpGet()]
+        public List<int> Get()
+        {
+            List<int> num = [];
+            int i = 0;
+            while (i <= 100)
+            {
+                if (i%2 == 0)
+                {
+                    num.Add(i);
+                    i++;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            return num;
+        }
+    }
+}
