6a31d7fd612ae5c1e844afe09a5ab78e1eb5fb04
pedrogasparini <pedrogasparini99@gmail.com>
2024-04-11 15:48:00 -0300

    Ejercicio 4 LINQ

---
+++ Web/Controllers/Ej4LinqController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej4LinqController : ControllerBase
+    {
+        [HttpGet()]
+        public List<int> Get([FromQuery] List<int> numbers)
+        {
+
+            var query =
+                from n in numbers
+                where n > 0
+                orderby n ascending
+                select n;
+
+            return query.ToList();
+        }
+    }
+}
+
