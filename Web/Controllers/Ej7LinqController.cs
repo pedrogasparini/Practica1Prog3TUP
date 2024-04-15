d98850bdf39d86f317931a5b8fafcd677610082d
Pedro Ortiz Gasparini <pedrogasparini99@gmail.com>
2024-04-15 18:51:08 -0300

    Ejercicio 7 LINQ

---
+++ Web/Controllers/Ej7LinqController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej7LinqController : ControllerBase
+    {
+        [HttpGet()]
+        public IList<string> Get([FromQuery] string cadena)
+        {
+            return DevolverPalabrasCapitales(cadena);
+        }
+        public static IList<string> DevolverPalabrasCapitales(string cadena)
+        {
+            var word = cadena.Split(" ");
+
+            var query =
+                from w in word
+                where w == (w.ToUpper())
+                select w;
+
+            return query.ToList();
+        }
+    }
+}
